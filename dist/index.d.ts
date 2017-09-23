4543e783e980c4c150192002e92fc6a2e227e606
baio <max.putilov@gmail.com>
2017-09-23 15:27:55 +0300

    fix build

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export * from "./graphql";
-import * as _http from "./http";
-import * as _futureUtils from "./future-utils";
-export declare const http: typeof _http;
-export declare const futureUtils: typeof _futureUtils;
-export declare const utils: typeof _futureUtils;
-export { Request as HttpRequest, RequestGet as HttpRequestGet, RequestMethods as HttpRequestMethods, RequestPost as HttpRequestPost, RequestPromise as HttpRequestPromise } from "./http";
-export { ReaderF } from "./future-utils";
--- dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const logt: (t: any) => any;
-export declare const log: any;
-export declare const mapObj: (f: any) => any;
-export declare const cleanObj: any;
-export declare const fmerge: any;
